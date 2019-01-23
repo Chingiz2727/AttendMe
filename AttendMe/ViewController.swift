238c491a62d734d8a93dd19c997841e1b0b0d260
Chingiz2727 <k.chyngiz@gmail.com>
2019-01-23 14:37:21 +0600

    Initial Commit

---
+++ AttendMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AttendMe
+//
+//  Created by Чингиз on 1/23/19.
+//  Copyright © 2019 Чингиз. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
